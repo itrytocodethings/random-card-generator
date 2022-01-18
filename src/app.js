a12480699687ebe65e03e0be3593e198b9afa4e6
itrytocodethings <imwaynebailey@gmail.com>
2022-01-18 00:17:42 +0000

    base random card gen complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNum = getRandomIntInclusive(2, 10);
+  let randomSuit = getRandomIntInclusive(1, 4);
+  let cardElement = document.querySelector(".card");
+  let suitIcons = document.querySelectorAll(".suit-icon");
+  let cardNumElement = document.querySelector(".card-num");
+
+  cardNumElement.innerHTML = cardNum;
+
+  if (randomSuit == 1) {
+    randomSuit = "hearts";
+    suitIcons.forEach(element => {
+      element.innerHTML = "♥";
+      element.style.color = "red";
+    });
+  }
+  if (randomSuit == 2) {
+    randomSuit = "diamonds";
+    suitIcons.forEach(element => (element.innerHTML = "♦"));
+  }
+  if (randomSuit == 3) {
+    randomSuit = "spade";
+    suitIcons.forEach(element => (element.innerHTML = "♠"));
+  }
+  if (randomSuit == 4) {
+    randomSuit = "clubs";
+    suitIcons.forEach(element => (element.innerHTML = "♣"));
+  }
 };
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
