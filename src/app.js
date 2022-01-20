6cb4d13e918c8fb930754a128498c8e7a0fe6bba
itrytocodethings <imwaynebailey@gmail.com>
2022-01-20 01:09:39 +0000

    more clean up/removed unnecessary variables

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let countElement = document.querySelector(".count");
-  let counterElement = document.querySelector(".counter");
   let count = 10;
   let counter = setInterval(generateCard, 10000);
   let countdown = setInterval(() => {
     count--;
-    countElement.innerHTML = `${count}`;
+    document.querySelector(".count").innerHTML = `${count}`;
     if (count == 0) count = 10;
   }, 1000);
   generateCard();
@@ -21,23 +19,21 @@ window.onload = function() {
   btn.addEventListener("click", e => {
     clearInterval(counter);
     clearInterval(countdown);
-    counterElement.style.display = "none";
+    document.querySelector(".counter").style.display = "none";
     let cardWidth = document.querySelector("#cardWidthInput").value;
     if (!cardWidth || !Number.isInteger(parseInt(cardWidth))) cardWidth = "23";
     generateCard(cardWidth);
   });
 
   function generateCard(width = "23") {
-    let cardElement = document.querySelector(".card");
     let cardArr = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+    let card = getRandomIntInclusive(0, cardArr.length - 1);
     let suitArr = ["hearts", "spades", "clubs", "diamonds"];
     let randomSuit = suitArr[getRandomIntInclusive(0, suitArr.length - 1)];
     let suitIcons = document.querySelectorAll(".suit-icon");
-    let cardNumElement = document.querySelector(".card-num");
-    let card = getRandomIntInclusive(0, cardArr.length - 1);
 
-    cardElement.style.maxWidth = `${width}rem`;
-    cardNumElement.innerHTML = cardArr[card];
+    document.querySelector(".card").style.maxWidth = `${width}rem`;
+    document.querySelector(".card-num").innerHTML = cardArr[card];
 
     if (randomSuit == "hearts") {
       suitIcons.forEach(element => {
